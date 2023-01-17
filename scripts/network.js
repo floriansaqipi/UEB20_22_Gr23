8da31ac99d2bf4a0335fa6fad05f7241575c26f8
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-01-17 11:51:30 +0100

    added-dynamic-pictures-fixed-filter

--- scripts/network.js
+++ scripts/network.js
@@ -1,16 +1,16 @@
 const users = [
     { id: 1, name: 'Rigoberto Whitney', location: 'Prishtine', position: 'Product Manager', workEnviorment: 'Physical', experience: 'Senior', education: 'Master', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 2, name: 'Keagan Hardin', location: 'Bucharest', position: 'UI/UX Designer', workEnviorment: 'Physical', experience: 'Senior', education: 'Bachelor', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 3, name: 'Hannah Winters', location: 'Berlin', position: 'Frontend Developer', workEnviorment: 'Online', experience: 'Junior', education: 'Bachelor', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 4, name: 'Tyrese Ferrell', location: 'New York', position: 'Backend Developer', workEnviorment: 'Physical', experience: 'Senior', education: 'Master', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 5, name: 'Erica Howard', location: 'Prishtine', position: 'Backend Developer', workEnviorment: 'Online', experience: 'Junior', education: 'Master', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 6, name: 'Paige Elliott', location: 'Bucharest', position: 'Frontend Developer', workEnviorment: 'Physical', experience: 'Senior', education: 'Bachelor', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 7, name: 'Chace Bullock', location: 'Berlin', position: 'Product Manager', workEnviorment: 'Physical', experience: 'Intern', education: 'High School', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 8, name: 'Elizabeth Reeves', location: 'New York', position: 'UI/UX Designer', workEnviorment: 'Online', experience: 'Intern', education: 'Master', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 9, name: 'Rigoberto Whitney', location: 'Prishtine', position: 'Frontend Developer', workEnviorment: 'Online', experience: 'Senior', education: 'Bachelor', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 10, name: 'Sherlyn Galvan', location: 'Oslo', position: 'Frontend Developer', workEnviorment: 'Physical', experience: 'Junior', education: 'Master', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 11, name: 'Brayden Herman', location: 'New York', position: 'Backend Developer', workEnviorment: 'Physical', experience: 'Senior', education: 'Master', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
-    { id: 12, name: 'Nikhil Duarte', location: 'Prishtine', position: 'Backend Developer', workEnviorment: 'Online', experience: 'Intern', education: 'High School', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/1.jpg' },
+    { id: 2, name: 'Keagan Hardin', location: 'Bucharest', position: 'UI/UX Designer', workEnviorment: 'Physical', experience: 'Senior', education: 'Bachelor', wallpaperSrc: './pictures/cover/2.jpg', profilePicSrc: './pictures/profile/2.jpg' },
+    { id: 3, name: 'Hannah Winters', location: 'Berlin', position: 'Frontend Developer', workEnviorment: 'Online', experience: 'Junior', education: 'Bachelor', wallpaperSrc: './pictures/cover/3.jpg', profilePicSrc: './pictures/profile/3.jpg' },
+    { id: 4, name: 'Tyrese Ferrell', location: 'New York', position: 'Backend Developer', workEnviorment: 'Physical', experience: 'Senior', education: 'Master', wallpaperSrc: './pictures/cover/6.jpg', profilePicSrc: './pictures/profile/4.jpg' },
+    { id: 5, name: 'Eric Howard', location: 'Prishtine', position: 'Backend Developer', workEnviorment: 'Online', experience: 'Junior', education: 'Master', wallpaperSrc: './pictures/cover/4.jpg', profilePicSrc: './pictures/profile/5.jpg' },
+    { id: 6, name: 'Paige Elliott', location: 'Bucharest', position: 'Frontend Developer', workEnviorment: 'Physical', experience: 'Senior', education: 'Bachelor', wallpaperSrc: './pictures/cover/5.jpg', profilePicSrc: './pictures/profile/6.jpg' },
+    { id: 7, name: 'Chace Bullock', location: 'Berlin', position: 'Product Manager', workEnviorment: 'Physical', experience: 'Intern', education: 'High School', wallpaperSrc: './pictures/cover/2.jpg', profilePicSrc: './pictures/profile/3.jpg' },
+    { id: 8, name: 'Scott Reeves', location: 'New York', position: 'UI/UX Designer', workEnviorment: 'Online', experience: 'Intern', education: 'Master', wallpaperSrc: './pictures/cover/6.jpg', profilePicSrc: './pictures/profile/2.jpg' },
+    { id: 9, name: 'Anton Wall', location: 'Prishtine', position: 'Frontend Developer', workEnviorment: 'Online', experience: 'Senior', education: 'Bachelor', wallpaperSrc: './pictures/cover/3.jpg', profilePicSrc: './pictures/profile/4.jpg' },
+    { id: 10, name: 'Sherlyn Galvan', location: 'Oslo', position: 'Frontend Developer', workEnviorment: 'Physical', experience: 'Junior', education: 'Master', wallpaperSrc: './pictures/cover/1.jpg', profilePicSrc: './pictures/profile/5.jpg' },
+    { id: 11, name: 'Brayden Herman', location: 'New York', position: 'Backend Developer', workEnviorment: 'Physical', experience: 'Senior', education: 'Master', wallpaperSrc: './pictures/cover/4.jpg', profilePicSrc: './pictures/profile/1.jpg' },
+    { id: 12, name: 'Nikhil Duarte', location: 'Prishtine', position: 'Backend Developer', workEnviorment: 'Online', experience: 'Intern', education: 'High School', wallpaperSrc: './pictures/cover/5.jpg', profilePicSrc: './pictures/profile/6.jpg' },
 ]
 
 const stateUsers = [...users];
@@ -60,16 +60,3 @@ for (let i = 0; i < cards.length; i++) {
     updateCardAttributes(cards[i].children[2], users[i].location, users[i].workEnviorment, users[i].experience, users[i].education);
 }
 
-
-
-// Anton Wall
-// Keagan Hardin
-// Hannah Winters
-// Tyrese Ferrell
-// Erica Howard
-// Paige Elliott
-// Chace Bullock
-// Elizabeth Reeves
-// Sherlyn Galvan
-// Brayden Herman
-// Nikhil Duarte
